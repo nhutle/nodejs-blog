c93c5d6f9202cefe0880747410c0c1f88e3889dc
Nhut Le <nhut.leminh@asnet.com.vn>
2015-06-12 18:59:40 +0700

    update script files

--- flightplan.js
+++ flightplan.js
@@ -21,42 +21,45 @@ plan.target('production', [{
   privateKey: '/home/vagrant/.ssh/id_rsa'
 }]);
 
-plan.remote('deploy', function(transport) {
-  transport.log('----- Move folder to root...');
+plan.remote('install', function(remote) {
+  remote.log('----- Install...');
+  remote.exec('cd nodejs-blog/scripts && sh ./install.sh');
 });
 
-plan.local('deploy', function(transport) {
-  transport.log('----- Move folder to root...');
+plan.remote('server', function(remote) {
+  remote.log('----- Server...');
+  remote.exec('cd nodejs-blog/scripts && ./server.sh');
 });
 
-plan.remote(['default', 'deploy'], function(transport) {
-
+plan.remote('deploy', function(remote) {
+  remote.log('----- Deploy...');
+  remote.exec('cd nodejs-blog/scripts && ./deploy.sh');
 });
 
-// run commands on localhost
-plan.local(['default', 'deploy'], function(transport) {
-  // nothing
+plan.remote('init', function(remote) {
+  remote.log('----- Initialize...');
+  remote.with('cd nodejs-blog/scripts', function(){
+    remote.exec('chmod +x init.sh && ./init.sh')
+  });
 });
 
-// run commands on remote hosts (destinations)
-// plan.remote(['default', 'cloneSource'], function(remote) {
-//   remote.log('----- Move folder to root...');
-// });
-//
 plan.remote('cloneSource', function(remote) {
   remote.log('----- Clone source code from github...');
   remote.exec('git clone -b master https://github.com/nhutle/nodejs-blog.git');
 });
 
 plan.remote('pullSource', function(remote) {
-  remote.log('----- Clone source code from github...');
+  remote.log('----- Pull source code from github...');
   remote.exec('cd nodejs-blog && git pull origin master');
 });
 
-plan.remote('init', function(remote) {
-  remote.log('----- Initialize...');
-  remote.exec('cd nodejs-blog/scripts && sh init.sh');
-  // remote.exec('ls -la');
-  // remote.exec('init.sh');
-  // remote.exec('sh init.sh');
+// run commands on remote server
+plan.remote(function(remote) {
+  // nothing
 });
+
+// run commands on localhost
+plan.local(function(local) {
+  // nothing
+});
+
