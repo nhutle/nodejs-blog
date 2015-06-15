dff7cce8158cccc62416772d445368e8c6e14f7e
Nhut Le <nhut.leminh@asnet.com.vn>
2015-06-15 10:43:23 +0700

    update deploy.sh script to fix bower's error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,16 +8,13 @@ if [ ! "$branch" ]
       echo "branch=master"
 fi
 
-# echo "### Pull code from github..."
-#git clone -b master https://github.com/nhutle/nodejs-blog.git
-
 echo "### Switch to a branch..."
 git checkout -b "$branch"
 git pull
 
 echo "### Install needed packages on front-end..."
 cd "../blog/frontend"
-bower install --allow-root
+sudo bower install --allow-root
 sudo npm install
 
 echo "### Build front-end..."
